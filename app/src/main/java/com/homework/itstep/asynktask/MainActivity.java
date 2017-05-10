b4596fde32252d29ca7f0f5a16610c8aceab25d6
ermeichook <ruslan.ermeichoo@gmail.com>
2017-05-10 13:56:55 +0300

    +urlconnection

--- app/src/main/java/com/homework/itstep/asynktask/MainActivity.java
+++ app/src/main/java/com/homework/itstep/asynktask/MainActivity.java
@@ -1,83 +1,36 @@
 package com.homework.itstep.asynktask;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.FrameLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
-import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static String TAG = MainActivity.class.getSimpleName();
-
-    private View container;
     private TextView tv;
-
-    private User user = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        container = findViewById(R.id.container);
-        tv = (TextView) findViewById(R.id.tv_name);
+        tv = (TextView) findViewById(R.id.tv_repsonse);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-
-        MyAsyncTask myAsyncTask = new MyAsyncTask("Hello", this);
-        myAsyncTask.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, 1);
-
-//        try {
-//            user = myAsyncTask.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, 1).get();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } catch (ExecutionException e) {
-//            e.printStackTrace();
-//        }
-
-        Log.d("Fsds", "Resume " + Thread.currentThread().getName());
+        MyAsyncTask myAsyncTask = new MyAsyncTask(this);
+        myAsyncTask.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
 
     }
 
-    public void showProgressBar(){
-        container.setVisibility(View.VISIBLE);
-    }
-
-    public void hideProgressBar(){
-        container.setVisibility(View.GONE);
-    }
-
-    public void onUserReceived(final User user){
-
-
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                String currentText = tv.getText().toString();
-                currentText = currentText + "\n" + "Name: " + user.name + " Age: " + user.age;
-                tv.setText(currentText);
-            }
-        });
-
-    }
-
-    public static class User{
-        String name;
-        int age;
-
-        public User(String name, int age) {
-            this.name = name;
-            this.age = age;
+    protected void updateView(String info) {
+        if (info != null) {
+            tv.setText(info);
+        } else {
+            tv.setText("problem downloading");
         }
     }
-
-
 }
--- app/src/main/java/com/homework/itstep/asynktask/MyAsyncTask.java
+++ app/src/main/java/com/homework/itstep/asynktask/MyAsyncTask.java
@@ -1,77 +1,90 @@
 package com.homework.itstep.asynktask;
 
 import android.os.AsyncTask;
-import android.util.Log;
-
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by Mike on 04.05.2017.
  */
 
-public class MyAsyncTask extends AsyncTask<Integer, Integer, Void> {
 
-    private final static String TAG = MyAsyncTask.class.getSimpleName();
+
+
+public class MyAsyncTask extends AsyncTask<Void, Void, String> {
 
     private MainActivity mActivity;
 
-
-    public MyAsyncTask(String s, MainActivity mainActivity) {
-        Log.d("Fsds", "AsyncTask created" + s);
-        mActivity = mainActivity;
-    }
-
-
-
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-
-        Log.d("Fsds", "On PreExecute " + Thread.currentThread().getName());
-        mActivity.showProgressBar();
-
-
-    }
-
-
-    @Override
-    protected void onProgressUpdate(Integer... values) {
-        super.onProgressUpdate(values);
-
+    public MyAsyncTask(MainActivity mActivity) {
+        this.mActivity = mActivity;
     }
 
     @Override
-    protected Void doInBackground(Integer[] params) {
+    protected String doInBackground(Void... params) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
 
-        Log.d("Fsds", "Do in Background" + Thread.currentThread().getName());
-
-        MainActivity.User user = null;
+        String jsonStr = null;
 
         try {
 
-            for (int i = 0; i < 5; i++) {
-                Thread.sleep(1000);
-                //new MainActivity.User("Vasya" + i, 10 + new Random().nextInt() * 10);
-                publishProgress(i);
-                mActivity.onUserReceived(new MainActivity.User("Vasya" + i, 10 + new Random().nextInt() * 10));
+            URL url = new URL("https://en.wikipedia.org/w/api.php?action=query&titles=Android&prop=revisions&rvprop=content&format=json");
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if(inputStream == null){
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null){
+                buffer.append(line + "\n");
             }
 
-        } catch (InterruptedException e) {
+            if (buffer.length() == 0){
+                return null;
+            }
+
+            jsonStr = buffer.toString();
+
+            return jsonStr;
+
+        } catch (MalformedURLException e) {
             e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (urlConnection != null){
+                urlConnection.disconnect();
+            }
+            if(reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
-        Log.d("Fsds", "Job Done " + Thread.currentThread().getName());
-        return null;
+
     }
 
     @Override
-    protected void onPostExecute(Void v) {
-        super.onPostExecute(v);
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
 
-        Log.d("Fsds", "OnPostExecute " + Thread.currentThread().getName());
-        mActivity.hideProgressBar();
-        //mActivity.onUserReceived(user);
-
-
+        mActivity.updateView(s);
     }
 }
